bc9c56357994dc8854c5c0d101a4de515230d4dc
Wahab-Al <dev70tec@gmail.com>
2026-01-03 03:55:11 +0100

    add tests

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const greet = (name) =>  `hello ${name}`
+
+mdule.exports = greet
+
+if (requir.main === module){
+  console.log(greet('world'));
+}
---
+++ src/test.sh
@@ -0,0 +1,17 @@
+EXPECTED="Heloo, Test!"
+
+OUTPUT=$(node -e "console.log(require('./src/app) ('Test'))")
+
+if ["$OUTPUT" == "$EXPECTED"]; then
+
+echo " Test passed"
+
+exit 0 
+
+else 
+
+echo "Test faild EXPECTED '$EXPECTED'but got '$OUTPUT'"
+
+exit 1 
+
+fi
